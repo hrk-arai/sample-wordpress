fdc251b9e640158ccaffb66d0dad5fc2293d0a68
h-arai <h-arai@creationline.com>
2016-07-12 18:03:27 +0900

    modify default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe wordpress::apache
-include_recipe wordpress::php
-include_recipe wordpress::mysql
-include_recipe wordpress::wordpress
+include_recipe sample-wordpress::apache
+include_recipe sample-wordpress::php
+include_recipe sample-wordpress::mysql
+include_recipe sample-wordpress::wordpress
